c71798a68e3ba51aed7f35781dc67cedca311dd2
Ernie White <ernie.white.68@gmail.com>
2026-06-27 14:30:33 -0500

    Add __init__.py to initialize levels module and import game levels

---
+++ levels/__init__.py
@@ -0,0 +1,27 @@
+from . import level_01_basics
+from . import level_02_cleaning
+from . import level_03_filtering
+from . import level_04_groupby
+from . import level_05_dates
+from . import level_06_merging
+from . import level_07_pivot
+from . import level_08_strings
+from . import level_09_rolling
+from . import level_10_duplicates
+from . import level_11_chunking
+from . import level_12_final_boss
+
+LEVELS = [
+    level_01_basics,
+    level_02_cleaning,
+    level_03_filtering,
+    level_04_groupby,
+    level_05_dates,
+    level_06_merging,
+    level_07_pivot,
+    level_08_strings,
+    level_09_rolling,
+    level_10_duplicates,
+    level_11_chunking,
+    level_12_final_boss,
+]
